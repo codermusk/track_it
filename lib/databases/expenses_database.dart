cf59db2a253675c7833b1bbc02676c7176071ebd
bharath <bharathbk2121@gmail.com>
2023-07-16 14:44:48 +0530

    Changes Directory name and maintaining the models and helpers properly

--- lib/databases/expenses_database.dart
+++ lib/databases/expenses_database.dart
@@ -1,25 +1,29 @@
-class DatabaseHelper {
-  static final _databaseName = 'Track_It.db';
-  static final _databaseVersion = 1 ;
+class ExpensesDataBase {
+  static final _databaseName = 'expenses.db';
+  static final _databaseVersion = 1;
+
 // This Private constructor is used to maintain a single instance of the database class in order to maintain singleton instance
   DatabaseHelper._private_constructor();
+
   static final DatabaseHelper instance = DatabaseHelper._private_constructor();
 
   static Database? _database;
+
 // This get methods automatically gets called during instantiating the database helper class
-  Future get database async{
+  Future get database async {
     if (_database != null) return _database!;
     _database = await _initDatabase();
-    return _database ;
+    return _database;
   }
 
-  _initDatabase() async{
+  _initDatabase() async {
     Directory path = getApplicationDocumentsDirectory();
     String path = join(path.path, _databaseName);
-    return await openDatabase(path, version: _databaseVersion, onCreate: _create);
+    return await openDatabase(
+        path, version: _databaseVersion, onCreate: _create);
   }
 
-  Future _create(Database db , int version) async{
+  Future _create(Database db, int version) async {
 
   }
 
